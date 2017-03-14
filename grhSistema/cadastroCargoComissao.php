baf47a51f3371aa6cbbea21d5c1946e41cd52623
Alat <alataguia@gmail.com>
2017-03-14 12:09:44 -0300

    algumas melhorias

--- grhSistema/cadastroCargoComissao.php
+++ grhSistema/cadastroCargoComissao.php
@@ -330,7 +330,7 @@ if($acesso)
                                        LEFT JOIN tbcomissao ON(tbservidor.idServidor = tbcomissao.idServidor)
                                             JOIN tbtipocomissao ON(tbcomissao.idTipoComissao=tbtipocomissao.idTipoComissao)
                        WHERE tbtipocomissao.ativo and tbtipocomissao.idTipoComissao = '.$id.'                    
-                  ORDER BY 7, 6, 4 desc';
+                  ORDER BY 7, tbcomissao.descricao, 4 desc';
 
             $result = $servidor->select($select);
             $label = array('IdFuncional','Matrícula','Nome','Nomeação','Exoneração','Nome do Cargo');

2e8f3d58c577e24ad3b730860f0be8dc292c9ce6
andreAguia <alataguia@gmail.com>
2017-04-24 22:38:47 -0300

    area de ferias

--- _classes/class.checkup.php
+++ _classes/class.checkup.php
@@ -886,4 +886,71 @@ class Checkup
     }
 
     ###########################################################
+    
+     /**
+     * Método get_servidorEstatutarioSemConcurso
+     * 
+     * Servidor Concursado se concurso cadastrado
+     */
+    
+    public function get_servidorEstatutarioSemConcurso()
+    {
+        $servidor = new Pessoal();
+        $metodo = explode(":",__METHOD__);
+
+        $select = 'SELECT idfuncional,
+                          matricula,
+                          tbpessoa.nome,
+                          tbperfil.nome,                          
+                          idServidor,
+                          idServidor,
+                          tbsituacao.situacao,
+                          idServidor
+                     FROM tbservidor LEFT JOIN tbpessoa USING (idPessoa)
+                                     LEFT JOIN tbperfil USING (idPerfil)
+                                     LEFT JOIN tbsituacao ON (tbservidor.situacao = tbsituacao.idSituacao)
+                    WHERE idConcurso is NULL
+                      AND tbservidor.situacao = 1
+                      AND idPerfil = 1
+                 ORDER BY tbpessoa.nome';
+
+        $result = $servidor->select($select);
+        $count = $servidor->count($select);
+
+        # Cabeçalho da tabela
+        $label = array('IdFuncional','Matrícula','Nome','Perfil','Lotação','Cargo','Situação');
+        $align = array('center','center','left','center','left','left','center');
+        $titulo = 'Servidores estatutário sem concurso cadastrado';
+        $classe = array(null,null,null,null,"Pessoal","Pessoal");
+        $metodo2 = array(null,null,null,null,"get_lotacao","get_cargo");
+        #$funcao = array(null,null,"date_to_php");
+        $linkEditar = 'servidor.php?fase=editar&id=';
+
+        # Exibe a tabela
+        $tabela = new Tabela();
+        $tabela->set_conteudo($result);
+        $tabela->set_label($label);
+        $tabela->set_align($align);
+        $tabela->set_titulo($titulo);
+        $tabela->set_classe($classe);
+        $tabela->set_metodo($metodo2);
+        #$tabela->set_funcao($funcao);
+        $tabela->set_editar($linkEditar);
+        $tabela->set_idCampo('idServidor');
+       
+        if ($count <> 0){
+            if($this->lista){
+                $tabela->show();
+                set_session('alertas',$metodo[2]);
+            }else{
+                $link = new Link($count.' '.$titulo,"?fase=alertas&alerta=".$metodo[2]);
+                $link->set_id("checkupResumo");
+                echo "<li>";
+                $link->show();
+                echo "</li>";
+            }
+        }
+    }
+
+    ###########################################################
 }

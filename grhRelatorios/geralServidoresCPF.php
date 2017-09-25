2f4a836b5693c53add0e51812fa0a04478d79390
Alat <alataguia@gmail.com>
2017-09-25 12:01:15 -0300

    - Inclusão de relatório solicitado pela dic

---
+++ grhRelatorios/geralServidoresCPF.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Sistema GRH
+ * 
+ * Relatório
+ *   
+ * By Alat
+ */
+
+# Servidor logado 
+$idUsuario = NULL;
+
+# Configuração
+include ("../grhSistema/_config.php");
+
+# Permissão de Acesso
+$acesso = Verifica::acesso($idUsuario,2);
+
+if($acesso)
+{    
+    # Conecta ao Banco de Dados
+    $servidor = new Pessoal();
+
+    # Começa uma nova página
+    $page = new Page();			
+    $page->iniciaPagina();
+
+    ######
+    
+    $select ='SELECT tbservidor.idFuncional,
+                     tbservidor.matricula,
+                     tbpessoa.nome,
+                     tbdocumentacao.cpf,
+                     tbperfil.nome
+                FROM tbservidor LEFT JOIN tbpessoa USING (idPessoa)
+                                     JOIN tbdocumentacao USING (idpessoa)
+                                     JOIN tbperfil USING (idPerfil)
+            ORDER BY tbpessoa.nome';
+
+    $result = $servidor->select($select);
+
+    $relatorio = new Relatorio();
+    $relatorio->set_titulo('Relatório Geral de Servidores Ativos e Inativos');
+    $relatorio->set_subtitulo('Ordenados pelo Nome');
+    $relatorio->set_label(array('IdFuncional','Matrícula','Nome','CPF','Perfil'));
+    #$relatorio->set_width(array(10,30,30,0,10,10,10));
+    $relatorio->set_align(array("center","center","left","cener"));
+    
+    $relatorio->set_conteudo($result);
+    $relatorio->show();
+
+    $page->terminaPagina();
+}

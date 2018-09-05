3141176eea9c6ba4b66a2178c56eeb520872733e
alat <alat@grh1206>
2018-09-05 10:20:41 -0300

    varias alteracoes

--- grhSistema/servidorPessoaisExtra.php
+++ grhSistema/servidorPessoaisExtra.php
@@ -11,19 +11,19 @@ $ano = $campoValor[8];
 if($ano == 0){
     $campoValor[8] = NULL;
     $ano = NULL;
-}
-
-# Força o ano a ter 4 digitos
-if(strlen($ano) <> 4){
-    $msgErro.='O ano tem que ter 4 dígitos!\n';
-    $erro = 1;
-}
-
-# Ano com 4 números
-if(strlen($ano) == 4){
-    # Ano futuro
-    if($ano > date('Y')){
-        $msgErro.='O ano de chegada nao pode ser no futuro!\n';
+}else{
+    # Força o ano a ter 4 digitos
+    if(strlen($ano) <> 4){
+        $msgErro.='O ano tem que ter 4 dígitos!\n';
         $erro = 1;
+    }
+
+    # Ano com 4 números
+    if(strlen($ano) == 4){
+        # Ano futuro
+        if($ano > date('Y')){
+            $msgErro.='O ano de chegada nao pode ser no futuro!\n';
+            $erro = 1;
+        }
     }
 }

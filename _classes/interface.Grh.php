683efa8bda4abccde3f15182925734397f6f0a9e
alat <alat@grh1206>
2018-09-21 14:49:19 -0300

    recadastramento

--- _classes/interface.Grh.php
+++ _classes/interface.Grh.php
@@ -222,8 +222,8 @@ class Grh{
             $botao->set_url('areaRecadastramento.php');
             $botao->set_image(PASTA_FIGURAS.'recadastramento.png',$tamanhoImage,$tamanhoImage);
             $botao->set_title('Recadastramento de Servidores');
-            $menu->add_item($botao);
-            #$menu->show();
+            #$menu->add_item($botao);
+            $menu->show();
             br();
             
             $grid->fechaColuna();

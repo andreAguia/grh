ff162acf04b68775a10e9f89cb02c3cb46f5e894
andre <andre@alat-pc>
2019-01-07 10:31:54 -0200

    Gestão de projetos

--- grhSistema/_config.php
+++ grhSistema/_config.php
@@ -96,7 +96,7 @@ setlocale(LC_ALL, 'pt_BR');
  * @param  $classe = a classe instanciada
  */
 
-function __autoload($classe){
+function autoload($classe){
     # Verifica se existe essa classe nas classes gerais
     if (file_exists(PASTA_CLASSES_GERAIS."/class.{$classe}.php")){
         include_once PASTA_CLASSES_GERAIS."/class.{$classe}.php";
@@ -144,6 +144,7 @@ function __autoload($classe){
         include_once PASTA_CLASSES_ADMIN."/interface.{$classe}.php";
     }
 }
+spl_autoload_register("autoload");
 
 # Sobre o Sistema
 $intra = new Intra();

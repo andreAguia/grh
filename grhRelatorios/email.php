32ffca294e39ff9575a2d88e48b07975f9ebbcd9
andre <andre@grh1206>
2019-06-10 11:30:04 -0300

    Importação de Parentes/dependentes

--- grhRelatorios/email.php
+++ grhRelatorios/email.php
@@ -24,28 +24,74 @@ if($acesso)
     # Começa uma nova página
     $page = new Page();			
     $page->iniciaPagina();
+    
+    # Pega os parâmetros dos relatórios
+    $lotacao = get('lotacao',post('lotacao'));
+    
+    $subTitulo = NULL;
 
     ######
     
     $select ='SELECT tbservidor.idfuncional,
                      tbpessoa.nome,
+                     concat(IFNULL(tblotacao.UADM,"")," - ",IFNULL(tblotacao.DIR,"")," - ",IFNULL(tblotacao.GER,"")) lotacao,
                      tbpessoa.emailUenf,
                      tbpessoa.emailPessoal
                 FROM tbservidor JOIN tbpessoa USING (idpessoa)
+                JOIN tbhistlot ON (tbservidor.idServidor = tbhistlot.idServidor)
+                JOIN tblotacao ON (tbhistlot.lotacao=tblotacao.idLotacao)
                WHERE tbservidor.situacao = 1
-                 AND idPerfil <> 10
-            ORDER BY tbpessoa.nome';
+                 AND tbservidor.idPerfil <> 10
+                 AND tbhistlot.data = (select max(data) from tbhistlot where tbhistlot.idServidor = tbservidor.idServidor)';
+    
+    if(!is_null($lotacao)){
+        # Verifica se o que veio é numérico
+        if(is_numeric($lotacao)){
+            $select .= ' AND (tblotacao.idlotacao = "'.$lotacao.'")';                
+            $subTitulo .= "Lotação: ".$servidor->get_nomeCompletoLotacao($lotacao)."<br/>";
+        }else{ # senão é uma diretoria genérica
+            $select .= ' AND (tblotacao.DIR = "'.$lotacao.'")';                
+            $subTitulo .= "Lotação: ".$lotacao."<br/>";
+        }
+    }
+        
+    $select .= ' ORDER BY tbpessoa.nome';
+            
 
     $result = $servidor->select($select);
 
     $relatorio = new Relatorio();
     $relatorio->set_titulo('Relatório de Emails dos Servidores Ativos');
-    $relatorio->set_subtitulo('Ordenados pelo Nome');
-    $relatorio->set_label(array('IdFuncional','Nome','Email UENF','Email Pessoal'));
+    $relatorio->set_subtitulo($subTitulo.'Ordenados pelo Nome');
+    $relatorio->set_label(array('IdFuncional','Nome','Lotação','Email UENF','Email Pessoal'));
     #$relatorio->set_width(array(10,40,50));
-    $relatorio->set_align(array("center","left","left","left"));
+    $relatorio->set_align(array("center","left","left","left","left"));
     
     $relatorio->set_conteudo($result);
+    
+    $listaLotacao = $servidor->select('(SELECT idlotacao, concat(IFNULL(tblotacao.DIR,"")," - ",IFNULL(tblotacao.GER,"")," - ",IFNULL(tblotacao.nome,"")) lotacao
+                                              FROM tblotacao
+                                             WHERE ativo) UNION (SELECT distinct DIR, DIR
+                                              FROM tblotacao
+                                             WHERE ativo)
+                                          ORDER BY 2');
+    
+    array_unshift($listaLotacao,array('*','-- Todos --'));
+    
+    $relatorio->set_formCampos(array(
+                               array ('nome' => 'lotacao',
+                                      'label' => 'Lotação:',
+                                      'tipo' => 'combo',
+                                      'array' => $listaLotacao,
+                                      'size' => 30,
+                                      'padrao' => $lotacao,
+                                      'title' => 'Mês',
+                                      'onChange' => 'formPadrao.submit();',
+                                      'linha' => 1)));
+    
+    $relatorio->set_formFocus('lotacao');
+    $relatorio->set_formLink('?');
+    
     $relatorio->show();
 
     $page->terminaPagina();
--- grhRelatorios/lotacaoServidoresAtivos.php
+++ grhRelatorios/lotacaoServidoresAtivos.php
@@ -38,8 +38,8 @@ if($acesso)
                      tbservidor.dtAdmissao,
                      tbservidor.idServidor
                 FROM tbservidor LEFT JOIN tbpessoa ON (tbservidor.idPessoa = tbpessoa.idPessoa)
-                                        JOIN tbhistlot ON (tbservidor.idServidor = tbhistlot.idServidor)
-                                        JOIN tblotacao ON (tbhistlot.lotacao=tblotacao.idLotacao)
+                                     JOIN tbhistlot ON (tbservidor.idServidor = tbhistlot.idServidor)
+                                     JOIN tblotacao ON (tbhistlot.lotacao=tblotacao.idLotacao)
                                    LEFT JOIN tbperfil ON (tbservidor.idPerfil = tbperfil.idPerfil)
                WHERE tbservidor.situacao = 1
                  AND tbservidor.idPerfil <> 10

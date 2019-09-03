0d8554b9d2c9fe8f14850965f02d0e6e5626ad9b
andre <andre@grh1206>
2019-09-03 14:38:24 -0300

    Listagem das prereitorias

--- grhRelatorios/lotacaoServidoresAtivosProReitorias.php
+++ grhRelatorios/lotacaoServidoresAtivosProReitorias.php
@@ -38,16 +38,19 @@ if($acesso)
                                         JOIN tbhistlot ON (tbservidor.idServidor = tbhistlot.idServidor)
                                         JOIN tblotacao ON (tbhistlot.lotacao = tblotacao.idLotacao)
                                    LEFT JOIN tbperfil ON (tbservidor.idPerfil = tbperfil.idPerfil)
+                                   LEFT JOIN tbcargo USING (idCargo)
+                                        JOIN tbtipocargo USING (idTipoCargo) 
                WHERE tbhistlot.data = (select max(data) from tbhistlot where tbhistlot.idServidor = tbservidor.idServidor)
                  AND (tbservidor.idPerfil = 1 OR tbservidor.idPerfil = 4)
                  AND situacao = 1
                  AND (idLotacao = 56 OR idLotacao = 57 OR idLotacao = 58)
+                 AND tbtipocargo.tipo = "Adm/Tec"
             ORDER BY lotacao, tbpessoa.nome';
 
     $result = $servidor->select($select);
 
     $relatorio = new Relatorio();
-    $relatorio->set_titulo('Relatório de Servidores Ativos das Pro-Reitorias');
+    $relatorio->set_titulo('Relatório de Servidores Administrativos e Técnicos Ativos das Pro-Reitorias');
     $relatorio->set_subtitulo('Agrupados por Lotação - Ordenados pelo Nome');
     $relatorio->set_label(array('IdFuncional','Nome','Cargo','Lotação','Perfil','Admissão','Situação'));
     $relatorio->set_width(array(10,30,30,0,10,10,10));

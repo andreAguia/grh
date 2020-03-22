b078aa1854a3bb9ffcb5e8f49d95157acbca2610
alat <alat@alat-pc>
2020-03-21 23:04:15 -0300

    Várias Alterações

--- grhSistema/cadastroMcf.php
+++ grhSistema/cadastroMcf.php
@@ -221,9 +221,9 @@ if($acesso){
                         $Objetolog->registraLog($idUsuario,$data,$atividade,NULL,$id,4);
 
                         # Volta para o menu
-                        #loadPage("?fase=listar");
+                        loadPage("?fase=listar");
                     }else{
-                        #loadPage("?fase=uploadMcf&id=.$id");
+                        loadPage("?fase=uploadMcf&id=.$id");
                     }
                 }
                 

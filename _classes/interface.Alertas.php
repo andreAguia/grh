df0b7d437cf31a0fe8f934423a8f94f796bc6ee1
andre <andre@pop-os.localdomain>
2021-03-24 11:19:08 -0300

    algumas alterações

--- _classes/interface.Alertas.php
+++ _classes/interface.Alertas.php
@@ -7,41 +7,41 @@ class Alertas {
      * 
      * @author André Águia (Alat) - alataguia@gmail.com
      */
+    private $itens = array(
+        array('Licenças', 'licencas'),
+        array('Triênio', 'trienio'),
+        array('Auxílio Creche', 'creche'),
+        array('Motorista', 'motorista'),
+        array('Aposentadoria', 'aposentadoria'),
+        array('Cadastro Geral', 'cadastro'),
+        array('Perfil', 'perfil'),
+        array('Concurso', 'concurso'),
+        array('Cargo em Comissão', 'comissao'),
+        array('Cedidos', 'cedidos'),
+        array('Férias', 'ferias'),
+        array('TRE', 'tre'),
+        array('Progressão', 'progressao'),
+        array('Benefícios', 'beneficios')
+    );
+
     ######################################################################################################################    
 
     public function menu($fase) {
 
-        $itens = array(
-            array('Licenças', 'licencas'),
-            array('Triênio', 'trienio'),
-            array('Auxílio Creche', 'creche'),
-            array('Motorista', 'motorista'),
-            array('Aposentadoria', 'aposentadoria'),
-            array('Cadastro Geral', 'cadastro'),
-            array('Perfil', 'perfil'),
-            array('Concurso', 'concurso'),
-            array('Cargo em Comissão', 'comissao'),
-            array('Cedidos', 'cedidos'),
-            array('Férias', 'ferias'),
-            array('TRE', 'tre'),
-            array('Progressão', 'progressao'),
-            array('Benefícios', 'beneficios')
-            
-        );
-        
         # Ordena as categorias
+
         function cmp($a, $b) {
             # Função específica que compara se $a é maior que $b
             return $a[0] > $b[0];
         }
-        
+
         // Ordena
-        usort($itens, 'cmp');
+        usort($this->itens, 'cmp');
 
         $menu = new Menu();
         $menu->add_item('titulo', 'Categorias');
 
-        foreach ($itens as $ii) {
+        foreach ($this->itens as $ii) {
             if ($fase == $ii[1]) {
                 $menu->add_item('link', '<b>| ' . $ii[0] . ' |</b>', '?fase=menu&categoria=' . $ii[1]);
             } else {
@@ -54,4 +54,19 @@ class Alertas {
     }
 
     ######################################################################################################################
+
+    public function getNomeCategoria($categoria) {
+
+        # Verifica se a categoria foi preenchida
+        if (empty($categoria)) {
+            return null;
+        }
+
+        foreach ($this->itens as $ii) {
+            if ($categoria == $ii[1]) {
+                return $ii[0];
+            }
+        }
+    }
+
 }

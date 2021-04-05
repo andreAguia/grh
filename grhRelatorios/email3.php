2adffd693e7742489f09c2b6f08c341eb79a3a67
andre <andre@pop-os.localdomain>
2021-04-05 16:58:56 -0300

    relatório de email3

--- grhRelatorios/email3.php
+++ grhRelatorios/email3.php
@@ -36,13 +36,15 @@ if ($acesso) {
     $relatorio = new Relatorio();
 
     $select = 'SELECT tbservidor.idServidor,
-                      tbservidor.idServidor,
+                      CONCAT(tbtipocargo.sigla," - ",tbcargo.nome),
                       concat(IFnull(tblotacao.UADM,"")," - ",IFnull(tblotacao.DIR,"")," - ",IFnull(tblotacao.GER,"")) lotacao,
                       tbpessoa.emailUenf,
                       tbpessoa.emailPessoal
                  FROM tbservidor JOIN tbpessoa USING (idpessoa)
-                 JOIN tbhistlot ON (tbservidor.idServidor = tbhistlot.idServidor)
-                 JOIN tblotacao ON (tbhistlot.lotacao=tblotacao.idLotacao)
+                                 JOIN tbcargo USING (idCargo)
+                                 JOIN tbtipocargo USING (idTipoCargo)
+                                 JOIN tbhistlot ON (tbservidor.idServidor = tbhistlot.idServidor)
+                                 JOIN tblotacao ON (tbhistlot.lotacao=tblotacao.idLotacao)                                 
                 WHERE tbservidor.situacao = 1
                   AND tbservidor.idPerfil <> 10
                   AND tbhistlot.data = (select max(data) from tbhistlot where tbhistlot.idServidor = tbservidor.idServidor)';
@@ -60,24 +62,24 @@ if ($acesso) {
     }
 
     if (is_null($lotacao)) {
-        $select .= ' ORDER BY tbpessoa.nome';
+        $select .= ' ORDER BY idTipoCargo, tbtipocargo.sigla, tbcargo.nome, tbpessoa.nome';
     } else {
-        $select .= ' ORDER BY DIR, GER, tbpessoa.nome';
+        $select .= ' ORDER BY DIR, GER, idTipoCargo, tbtipocargo.sigla, tbcargo.nome, tbpessoa.nome';
     }
 
 
     $result = $servidor->select($select);
 
     $relatorio->set_titulo('Relatório de Emails dos Servidores Ativos');
-    $relatorio->set_subtitulo($subTitulo . 'Ordenados pelo Nome');
+    $relatorio->set_subtitulo($subTitulo . 'Ordenados pelo Cargo');
     $relatorio->set_label(array('Servidor', 'Cargo', 'Lotação', 'Email UENF', 'Email Pessoal'));
     #$relatorio->set_width(array(20,20,20,20,20));
     $relatorio->set_align(array("left", "left", "left", "left", "left"));
     $relatorio->set_conteudo($result);
     #$relatorio->set_bordaInterna(true);
 
-    $relatorio->set_classe(array("pessoal", "pessoal"));
-    $relatorio->set_metodo(array("get_nome", "get_cargoSimples"));
+    $relatorio->set_classe(array("pessoal"));
+    $relatorio->set_metodo(array("get_nome"));
 
     $listaLotacao = $servidor->select('(SELECT idlotacao, concat(IFnull(tblotacao.DIR,"")," - ",IFnull(tblotacao.GER,"")," - ",IFnull(tblotacao.nome,"")) lotacao
                                               FROM tblotacao

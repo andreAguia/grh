f2ea4e800ffe390975943af04509df5e5c5c9174
andre <andre@pop-os.localdomain>
2021-05-10 11:24:07 -0300

    pequenas alterações

--- grhSistema/_config.php
+++ grhSistema/_config.php
@@ -5,15 +5,15 @@
  * 
  * By Alat
  */
-########################################################33
-# Iniciando a sessão
+
+/*
+ *  Iniciando a sessão
+ */
 
 if (session_status() !== PHP_SESSION_ACTIVE) { 
     session_cache_expire(300);
     session_start();
 }
-$cache_expire = session_cache_expire();
-########################################################
 
 /*
  *  Classes

1bb3261ba21abb28886acef5804af3b8243fa62b
andre <andre@pop-os.localdomain>
2021-09-02 11:22:40 -0300

    relatórico de historico de lotação

---
+++ grhRelatorios/lotacaoServidoresAtivosHistorico.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * Sistema GRH
+ * 
+ * Relatório
+ *   
+ * By Alat
+ */
+# Servidor logado 
+$idUsuario = null;
+
+# Configuração
+include ("../grhSistema/_config.php");
+
+# Permissão de Acesso
+$acesso = Verifica::acesso($idUsuario, 2);
+
+if ($acesso) {
+    # Conecta ao Banco de Dados
+    $servidor = new Pessoal();
+
+    # Começa uma nova página
+    $page = new Page();
+    $page->iniciaPagina();
+
+    # Pega os parâmetros dos relatórios
+    $lotacao = get('lotacao', post('lotacao'),$servidor->get_idLotacao($intra->get_idServidor($idUsuario)));
+
+    ######
+
+    $select = 'SELECT tbservidor.idFuncional,
+                     tbpessoa.nome,
+                     tbservidor.idServidor,
+                     concat(IFnull(tblotacao.UADM,"")," - ",IFnull(tblotacao.DIR,"")," - ",IFnull(tblotacao.GER,"")," - ",IFnull(tblotacao.nome,"")) lotacao,
+                     tbperfil.nome,
+                     tbhistlot.data,
+                     tbservidor.idServidor
+                FROM tbservidor LEFT JOIN tbpessoa ON (tbservidor.idPessoa = tbpessoa.idPessoa)
+                                     JOIN tbhistlot ON (tbservidor.idServidor = tbhistlot.idServidor)
+                                     JOIN tblotacao ON (tbhistlot.lotacao=tblotacao.idLotacao)
+                                   LEFT JOIN tbperfil ON (tbservidor.idPerfil = tbperfil.idPerfil)
+               WHERE tbservidor.situacao = 1';
+
+    # lotacao
+    if (!is_null($lotacao)) {
+        # Verifica se o que veio é numérico
+        if (is_numeric($lotacao)) {
+            $select .= ' AND (tblotacao.idlotacao =  "' . $lotacao . '")';
+        } else { # senão é uma diretoria genérica
+            $select .= ' AND (tblotacao.DIR = "' . $lotacao . '")';
+        }
+    }
+
+    $select .= ' ORDER BY lotacao, tbpessoa.nome';
+
+    $result = $servidor->select($select);
+
+    $relatorio = new Relatorio();
+    $relatorio->set_titulo('Histórico de Servidores Ativos Por Lotação');
+    $relatorio->set_subtitulo('Agrupados por Lotação - Ordenados pelo Nome');
+    $relatorio->set_label(array('IdFuncional', 'Nome', 'Cargo', 'Lotação', 'Perfil', 'Data da Transferência','Lotação atual'));
+    #$relatorio->set_width(array(10,30,30,0,10,10,10));
+    $relatorio->set_align(array("center", "left", "left"));
+    $relatorio->set_funcao(array(null, null, null, null, null, "date_to_php"));
+
+    $relatorio->set_classe(array(null, null, "pessoal",null, null,null, "pessoal"));
+    $relatorio->set_metodo(array(null, null, "get_Cargo",null, null,null, "get_lotacao"));
+
+    $relatorio->set_conteudo($result);
+    $relatorio->set_numGrupo(3);
+
+    $listaLotacao = $servidor->select('(SELECT idlotacao, concat(IFnull(tblotacao.DIR,"")," - ",IFnull(tblotacao.GER,"")," - ",IFnull(tblotacao.nome,"")) lotacao
+                                              FROM tblotacao
+                                             WHERE ativo) UNION (SELECT distinct DIR, DIR FROM tblotacao 
+                                             WHERE ativo)
+                                          ORDER BY 2');
+    $relatorio->set_formCampos(array(
+        array('nome' => 'lotacao',
+            'label' => 'Lotação:',
+            'tipo' => 'combo',
+            'array' => $listaLotacao,
+            'size' => 30,
+            'padrao' => $lotacao,
+            'title' => 'Mês',
+            'onChange' => 'formPadrao.submit();',
+            'linha' => 1)));
+
+    $relatorio->set_formFocus('lotacao');
+    $relatorio->set_formLink('?');
+    $relatorio->show();
+
+    $page->terminaPagina();
+}

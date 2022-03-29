8d6af328e2e0a5925db9ebe46008765e5e0bbb1f
andreAguia <alataguia@gmail.com>
2022-03-29 10:54:12 -0300

    bug do cadastro de salario

--- _classes/interface.MenuPrincipal.php
+++ _classes/interface.MenuPrincipal.php
@@ -856,7 +856,7 @@ class MenuPrincipal {
         $menu = new Menu("menuProcedimentos");
         #$menu->add_item('titulo','Tabelas Secundárias','#','Tabelas Secundárias');
         #$menu->add_item('link','Descrrição dos Cargos em Comissão','cadastroDescricaoComissao.php?grh=1','Acessa o Cadastro de Descrição dos Cargos em Comissão');
-        $menu->add_item('link', 'Tabela Salarial', 'cadastroTabelaSalarial.php?grh=1', 'Acessa o Cadastro de Tabela Salarial');
+        #$menu->add_item('link', 'Tabela Salarial', 'cadastroTabelaSalarial.php?grh=1', 'Acessa o Cadastro de Tabela Salarial');
         $menu->add_item('link', 'Banco', 'cadastroBanco.php?grh=1', 'Acessa o Cadastro de Bancos');
         $menu->add_item('link', 'Campus', 'cadastroCampus.php?grh=1', 'Acessa o Cadastro de Campus Universitários');
         $menu->add_item('link', 'Escolaridade', 'cadastroEscolaridade.php?grh=1', 'Acessa o Cadastro de Escolaridade');

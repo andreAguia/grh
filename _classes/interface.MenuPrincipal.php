48ec69910fd6aa1daa441c87c73080a180e5a030
andreAguia <alataguia@gmail.com>
2022-04-08 15:37:56 -0300

    correção do nome afastamento que estava errado

--- _classes/interface.MenuPrincipal.php
+++ _classes/interface.MenuPrincipal.php
@@ -252,7 +252,7 @@ class MenuPrincipal {
         # Menu
         $menu = new Menu("menuProcedimentos");
         $menu->add_item('titulo','Documentos Gerais','#','Área Especial');
-        $menu->add_item('linkWindow', 'Codigos de Afastemento', PASTA_DOCUMENTOS.'codigoAfastamento.pdf');
+        $menu->add_item('linkWindow', 'Codigos de Afastamento', PASTA_DOCUMENTOS.'codigoAfastamento.pdf');
         $menu->add_item('titulo','Legislação','#','Área Especial');
         $menu->add_item('linkWindow', 'Estatuto dos Servidores', "http://alerjln1.alerj.rj.gov.br/decest.nsf/968d5212a901f75f0325654c00612d5c/2caa8a7c2265c33b0325698a0068e8fb?OpenDocument#_Section1", "Decreto nº 2479 de 08 de Março de 1979");
         $menu->add_item('linkWindow', 'Plano de Cargos e Vencimentos', "http://alerjln1.alerj.rj.gov.br/contlei.nsf/b24a2da5a077847c032564f4005d4bf2/aa5390d4c58db774832571b60066a2ba?OpenDocument", "LEI Nº 4.800 de 29 de Junho de 2006");

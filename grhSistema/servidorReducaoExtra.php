905119eb264147f2202db11aa07d321bfe5fefce
André Águia <alataguia@gmail.com>
2023-03-14 16:26:16 -0300

    altera comportamento do status da rotina de redução

--- grhSistema/servidorReducaoExtra.php
+++ grhSistema/servidorReducaoExtra.php
@@ -7,6 +7,7 @@
 
 $resultado = $campoValor[2];
 $status = $campoValor[1];
+$dtPublicacao = date_to_php($campoValor[3]);
 $dtInicio = date_to_php($campoValor[5]);
 $periodo = $campoValor[6];
 
@@ -19,23 +20,33 @@ switch ($resultado) {
         $campoValor[1] = 1;
         break;
 
-    # Resultado: 1 - Deferido
+    # Se
+    # Resultado:          1 - Deferido
+    # Data de Publicação: not Null
+    # Então
     # Status:    2 - Vigente até o término do benefício, após essa data passa para 
     #            3 - Arquivado
     case 1:
 
-        # Verifica se já está cadastrada a data de início e o período
-        if ((is_null($dtInicio)) OR (is_null($periodo))) {
-            $campoValor[1] = 2;
-        } else {
-            $dtTermino = addMeses($dtInicio, $periodo);
+        # Verifica se a data de Publicação está preenchida
+        if (!empty($dtPublicacao)) {
 
-            # Verifica se a data de término já passou
-            if (jaPassou($dtTermino)) {
-                $campoValor[1] = 3; // Arquivado
+            # Verifica se já está cadastrada a data de início e o período
+            if ((is_null($dtInicio)) OR (is_null($periodo))) {
+                $campoValor[1] = 2;
             } else {
-                $campoValor[1] = 2; // Vigente
+                $dtTermino = addMeses($dtInicio, $periodo);
+
+                # Verifica se a data de término já passou
+                if (jaPassou($dtTermino)) {
+                    $campoValor[1] = 3; // Arquivado
+                } else {
+                    $campoValor[1] = 2; // Vigente
+                }
             }
+        }else{
+            # Continua em aberto
+            $campoValor[1] = 1;
         }
         break;
 
@@ -44,7 +55,7 @@ switch ($resultado) {
     case 2:
         $campoValor[1] = 3;
         break;
-        
+
     # Resultado: 3 - Interrompido
     # Status:    3 - Arquivado    
     case 3:

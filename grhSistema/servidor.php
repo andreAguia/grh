b2a40b57101f7bb99a84d6bd4f39b9a7cda179c7
andreAguia <alataguia@gmail.com>
2023-06-02 15:13:28 -0300

    retira estagiário e bolsistas das listas

--- grhSistema/servidor.php
+++ grhSistema/servidor.php
@@ -284,16 +284,6 @@ if ($acesso) {
             $controle->set_linha(2);
             $controle->set_col(3);
             $form->add_item($controle);
-
-            # submit
-            #$controle = new Input('submit','submit');
-            #$controle->set_valor('Pesquisar');
-            #$controle->set_size(20);
-            #$controle->set_accessKey('P');
-            #$controle->set_linha(3);
-            #$controle->set_col(2);
-            #$form->add_item($controle);
-
             $form->show();
 
             # Paginação

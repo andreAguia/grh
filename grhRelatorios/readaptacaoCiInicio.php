18ec8abbd1a664dbe930d2363175b6fc3f4d8b5d
andre <andre@grh-andre>
2023-07-10 14:56:47 -0300

    teste

--- grhRelatorios/readaptacaoCiInicio.php
+++ grhRelatorios/readaptacaoCiInicio.php
@@ -66,7 +66,7 @@ if ($acesso) {
 
     # Monta a CI
     $ci = new Ci($numCiInicio, $dtCiInicio, $assunto);
-    echo $pessoal->get_gerente(66);
+    echo $pessoal->get_nome($servidorGrh);
     
     # Verifica se alterou o servidor da GRH
     if ($servidorGrh <> $pessoal->get_gerente(66)) {

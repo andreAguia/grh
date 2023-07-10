a2ae230cdf9155f03b17d5126cdd26fbdf022c8a
andre <andre@grh-andre>
2023-07-10 14:21:21 -0300

    teste

--- grhRelatorios/readaptacaoCiInicio.php
+++ grhRelatorios/readaptacaoCiInicio.php
@@ -17,7 +17,6 @@ include ("../grhSistema/_config.php");
 $acesso = Verifica::acesso($idUsuario, [1, 2, 12]);
 
 if ($acesso) {
-    echo "teste1";
 
     # Conecta ao Banco de Dados
     $pessoal = new Pessoal();
@@ -61,32 +60,20 @@ if ($acesso) {
     $nomeServidor = $pessoal->get_nome($idServidorPesquisado);
     $idFuncional = $pessoal->get_idFuncional($idServidorPesquisado);
 
-    echo "teste2";
-
     # Assunto
     $assunto = "Readaptação de " . $nomeServidor;
 
     # Monta a CI
     $ci = new Ci($numCiInicio, $dtCiInicio, $assunto);
-    echo "teste3";
+    
     # Verifica se alterou o servidor da GRH
     if ($servidorGrh <> $pessoal->get_gerente(66)) {
-        echo "teste4";
-
-        echo $pessoal->get_nome($servidorGrh);
-        br();
-        echo $pessoal->get_cargoSimples($servidorGrh);
-        br();
-        echo $pessoal->get_idFuncional($servidorGrh);
-        br();
-
         $ci->set_nomeAssinatura(
-                $pessoal->get_nome($servidorGrh),
-                $pessoal->get_cargoSimples($servidorGrh),
+                $pessoal->get_nome($servidorGrh), 
+                $pessoal->get_cargoSimples($servidorGrh), 
                 $pessoal->get_idFuncional($servidorGrh));
-        echo "teste5";
     }
-
+    
     $ci->set_destinoNome($chefe);
     $ci->set_destinoSetor($cargo);
     $ci->set_texto('Vimos informar a concessão de <b>Readaptação</b> do(a) servidor(a) <b>' . strtoupper($nomeServidor) . '</b>,'

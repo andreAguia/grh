3b6e8e0bd154b3f7771c6884369acd43e851ddd4
andre <andre@grh-andre>
2023-08-24 16:50:42 -0300

    melhorias na area de readaptação

--- grhSistema/servidorReadaptacao.php
+++ grhSistema/servidorReadaptacao.php
@@ -126,7 +126,7 @@ if ($acesso) {
         $linkBotao3 = new Link("Procedimentos", "?fase=procedimentos");
         $linkBotao3->set_class('button');
         $linkBotao3->set_title('Regras da readaptação');
-        $linkBotao3->set_target("_blank");
+        $linkBotao3->set_target("_blank4");
         $menu->add_link($linkBotao3, "right");
 
         # Site

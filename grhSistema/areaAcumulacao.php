e65615003e5ccfc65b6971fd9b299538f473ec77
andre <andre@grh-andre>
2023-09-22 14:03:05 -0300

    acerto no menu da área dos benefícios

--- grhSistema/areaAcumulacao.php
+++ grhSistema/areaAcumulacao.php
@@ -100,13 +100,6 @@ if ($acesso) {
             $linkBotao3->set_title('Procedimentos de Acumulação');
             $linkBotao3->set_target("_blank");
             $menu1->add_link($linkBotao3, "right");
-            
-            # Site
-            $botaoSite = new Button("Site da GRH");
-            $botaoSite->set_target('_blank');
-            $botaoSite->set_title("Pagina no site da GRH sobre Redução da Carga Horária");
-            $botaoSite->set_url("https://uenf.br/dga/grh/gerencia-de-recursos-humanos/acumulacao-de-cargos/");
-            $menu1->add_link($botaoSite, "right");
 
             # Relatórios
             $imagem = new Imagem(PASTA_FIGURAS . 'print.png', null, 15, 15);

ba43ac1e2d10c9ae9ceff81d47e7e71fec1e5ef9
andre <andre@grh-andre>
2023-09-22 13:36:26 -0300

    acerto no menu da área dos benefícios

--- grhSistema/servidorReadaptacao.php
+++ grhSistema/servidorReadaptacao.php
@@ -129,13 +129,6 @@ if ($acesso) {
         $linkBotao3->set_target("_blank3");
         $menu->add_link($linkBotao3, "right");
 
-        # Site
-        $botaoSite = new Button("Site da GRH");
-        $botaoSite->set_target('_blank');
-        $botaoSite->set_title("Pagina da GRH");
-        $botaoSite->set_url("https://uenf.br/dga/grh/gerencia-de-recursos-humanos/readaptacao/");
-        $menu->add_link($botaoSite, "right");
-
         # Relatório
         $imagem = new Imagem(PASTA_FIGURAS . 'print.png', null, 15, 15);
         $botaoRel = new Button();

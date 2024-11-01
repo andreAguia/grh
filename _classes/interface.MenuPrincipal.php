9ad7f35ee26b3bcda652ab1cda74fedd368687b7
André Águia <alataguia@gmail.com>
2024-11-01 06:41:50 -0300

    termino da semana do servidor

--- _classes/interface.MenuPrincipal.php
+++ _classes/interface.MenuPrincipal.php
@@ -55,7 +55,7 @@ class MenuPrincipal {
         # Sispatri        
         #$this->moduloSispatri();
         # Eventos        
-        $this->moduloEventos();
+        #$this->moduloEventos();
 
         # sistemas
         $this->moduloSistemas();

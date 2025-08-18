7f241cf46bafce03697d47290c218a350863309a
André Águia <alataguia@gmail.com>
2025-08-18 16:43:25 -0300

    controle de formação

---
+++ grhSistema/cadastroMarcador.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+ * Cadastro de MArcador de formação
+ *  
+ * By Alat
+ */
+# Reservado para o servidor logado
+$idUsuario = null;
+
+# Configuração
+include ("_config.php");
+
+# Permissão de Acesso
+$acesso = Verifica::acesso($idUsuario, [1, 2, 12]);
+
+if ($acesso) {
+    # Conecta ao Banco de Dados
+    $intra = new Intra();
+    $pessoal = new Pessoal();
+
+    # Verifica a fase do programa
+    $fase = get('fase', 'listar');
+
+    # Verifica se veio menu grh e registra o acesso no log
+    $grh = get('grh', false);
+    if ($grh) {
+        # Grava no log a atividade
+        $atividade = "Visualizou o cadastro de Marcador para cadastro de Formação";
+        $data = date("Y-m-d H:i:s");
+        $intra->registraLog($idUsuario, $data, $atividade, null, null, 7);
+    }
+
+    # pega o id (se tiver)
+    $id = soNumeros(get('id'));
+
+    # Pega o parametro de pesquisa (se tiver)
+    $parametro = retiraAspas(post('parametro', get_session('sessionParametro')));
+    set_session('sessionParametro', $parametro);
+
+    # Começa uma nova página
+    $page = new Page();
+    $page->iniciaPagina();
+
+    # Cabeçalho da Página
+    AreaServidor::cabecalho();
+    br();
+
+    # Abre um novo objeto Modelo
+    $objeto = new Modelo();
+
+    ################################################################
+    # Nome do Modelo (aparecerá nos fildset e no caption da tabela)
+    $objeto->set_nome('Marcador');
+
+    # Botão de voltar da lista
+    $objeto->set_voltarLista('grh.php');
+
+    # controle de pesquisa
+    $objeto->set_parametroLabel('Pesquisar');
+    $objeto->set_parametroValue($parametro);
+    
+    # select da lista
+    $objeto->set_selectLista("SELECT idFormacaoMarcador,
+                                     marcador,
+                                     obs
+                                FROM tbformacaomarcador
+                               WHERE marcador LIKE '%{$parametro}%'
+                             ORDER BY 1 asc");
+
+    # select do edita
+    $objeto->set_selectEdita("SELECT marcador,
+                                     obs
+                                FROM tbformacaomarcador
+                               WHERE idFormacaoMarcador = {$id}");
+    
+    # Habilita o modo leitura para usuario de regra 12
+    if (Verifica::acesso($idUsuario, 12)) {
+        $objeto->set_modoLeitura(true);
+    }
+
+    # Caminhos
+    $objeto->set_linkEditar('?fase=editar');
+    #$objeto->set_linkExcluir('?fase=excluir');
+    $objeto->set_linkGravar('?fase=gravar');
+    $objeto->set_linkListar('?fase=listar');
+
+    # Parametros da tabela
+    $objeto->set_label(["Id", "Marcador", "Obs"]);
+    $objeto->set_width([5, 35, 40, 10]);
+    $objeto->set_align(["center", "center", "left"]);
+
+    # Classe do banco de dados
+    $objeto->set_classBd('Pessoal');
+
+    # Nome da tabela
+    $objeto->set_tabela('tbformacaomarcador');
+
+    # Nome do campo id
+    $objeto->set_idCampo('idFormacaoMarcador');
+    
+    # Campos para o formulario
+    $objeto->set_campos(array(
+        array('linha' => 1,
+            'nome' => 'marcador',
+            'label' => 'Marcador:',
+            'tipo' => 'texto',
+            'required' => true,
+            'autofocus' => true,
+            'size' => 100),
+        array('linha' => 2,
+            'nome' => 'obs',
+            'label' => 'Observação:',
+            'tipo' => 'textarea',
+            'size' => array(80, 5))));
+
+    # idUsuário para o Log
+    $objeto->set_idUsuario($idUsuario);
+
+    ################################################################
+    switch ($fase) {
+        case "" :
+        case "listar" :
+            $objeto->listar();
+            break;
+
+        case "editar" :
+        case "excluir" :
+        case "gravar" :
+            $objeto->$fase($id);
+            break;
+    }
+
+    $page->terminaPagina();
+} else {
+    loadPage("../../areaServidor/sistema/login.php");
+}

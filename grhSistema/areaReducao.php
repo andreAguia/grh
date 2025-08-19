0b15f58dead2e2666ccddb2b24ccb4e9ed47c626
André Águia <alataguia@gmail.com>
2025-08-19 16:17:00 -0300

    melhorias gerais

--- grhSistema/areaReducao.php
+++ grhSistema/areaReducao.php
@@ -96,7 +96,7 @@ if ($acesso) {
             $botaoCalendario = new Link("Calendário", "calendario.php");
             $botaoCalendario->set_class('button');
             $botaoCalendario->set_title('Exibe o calendário');
-            $botaoCalendario->set_target("_calenmdario");
+            $botaoCalendario->set_target("_calendario");
             $menu1->add_link($botaoCalendario, "right");
 
             # Procedimentos

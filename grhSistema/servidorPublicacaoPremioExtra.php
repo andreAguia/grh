8f4b1f274fe4b91efbb2e0315cb12126a002bd95
André Águia <alataguia@gmail.com>
2025-10-27 17:06:15 -0300

    melhorias na rotina de licença prêmio

--- grhSistema/servidorPublicacaoPremioExtra.php
+++ grhSistema/servidorPublicacaoPremioExtra.php
@@ -8,7 +8,11 @@
 $dtInicio = date_to_php($campoValor[1]);
 $dtFim = date_to_php($campoValor[2]);
 
+$licenca = new LicencaPremio();
+
+# Preenche a data final quando deixada em branco
 if (empty($dtFim)) {
+    # Define a data como sendo 5 anos menos 1 dia
     $dtFim = addAnos($dtInicio, 5);
     $dtFim = addDias($dtFim, -1, false);
     $campoValor[2] = date_to_bd($dtFim);
@@ -22,5 +26,18 @@ $anos = intval($dias / 365);
 # Informa se teve menos que 5 aos de período aquisitivo
 if ($anos < 5) {
     $erro = 1;
-    $msgErro .= 'O Período Aquisitivo não pode ser menor que 5 anos! ('.$dias.' dias).\n';
+    $msgErro .= 'O Período Aquisitivo não pode ser menor que 5 anos! (' . $dias . ' dias).\n';
+}
+
+# Verifica se a data inicial é posterior a data final do período anterior
+if (!empty($id)) {
+    
+    # Pega o valor da data do fim do período do periodo anterior
+    $dataAnterior = $licenca->get_dataFinalPeriodoAnterior($id);   
+    
+    
+    if (strtotime($campoValor[1]) <= strtotime(date_to_bd($dataAnterior))) {
+        $erro = 1;
+        $msgErro .= 'A Data Inicial do Período aquisitivo deve ser posterior a ' . $dataAnterior . '. Que foi a data de término do período anterior!.\n';
+    }
 }

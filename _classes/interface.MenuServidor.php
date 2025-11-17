f57b8ac66bb9467bc873016ed29758fc6b9f8c27
André Águia <alataguia@gmail.com>
2025-11-17 17:21:03 -0300

    declaração de atribuições detalhada

--- _classes/interface.MenuServidor.php
+++ _classes/interface.MenuServidor.php
@@ -656,6 +656,7 @@ class MenuServidor {
         $menu->add_item('linkWindow', 'Declaração de Inquérito Administrativo', '../grhRelatorios/declaracao.InqueritoAdministrativo.php');
         if ($this->perfil == 1) {
             $menu->add_item('linkWindow', 'Declaração de Atribuições do Cargo', '../grhRelatorios/declaracao.AtribuicoesCargo.php');
+            $menu->add_item('linkWindow', 'Declaração de Atribuições do Cargo - Detalhado', '../grhRelatorios/declaracao.AtribuicoesCargo2.php');
         }
 
         if ($this->situacao == "Ativo") {

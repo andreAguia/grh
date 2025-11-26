2b9237a313dd7681dfaf1937a0d23b8c1d203bbd
André Águia <alataguia@gmail.com>
2025-11-26 14:20:49 -0300

    erro na area de férias

--- grhSistema/areaFeriasExercicio2.php
+++ grhSistema/areaFeriasExercicio2.php
@@ -488,8 +488,6 @@ if ($acesso) {
                 $tabela->set_nomeColunaEditar("Acessar");
                 $tabela->set_editarBotao("olho.png");
                 $tabela->set_idCampo('idServidor');
-                $tabela->set_rowspan(0);
-                $tabela->set_grupoCorColuna(0);
                 $tabela->set_conteudo($resultDuplicado);
                 $tabela->show();
             }

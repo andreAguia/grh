6dff483f0314293682cb4fb63d0f4597c651ee92
André Águia <alataguia@gmail.com>
2025-11-26 15:08:11 -0300

    erro na area de férias

--- grhSistema/areaFeriasExercicio2.php
+++ grhSistema/areaFeriasExercicio2.php
@@ -476,7 +476,7 @@ if ($acesso) {
             if (!empty($resultDuplicado)) {
 
                 $tabela = new Tabela();
-                $tabela->set_titulo("Lançamentos Duplicados!!");
+                $tabela->set_titulo("Problemas encontrados!!");
                 $tabela->set_subtitulo("Lançamentos Duplicados");
                 $tabela->set_label(['Nome', 'Inicio', 'Dias']);
                 $tabela->set_width([70, 15, 10]);

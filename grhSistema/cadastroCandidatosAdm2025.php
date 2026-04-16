5d52bbdf123c19cdceec01cbcd0f7089e219450c
André Águia <alataguia@gmail.com>
2026-04-16 16:12:15 -0300

    melhorias na listagem de candidatos de todos os cargos

--- grhSistema/cadastroCandidatosAdm2025.php
+++ grhSistema/cadastroCandidatosAdm2025.php
@@ -489,7 +489,7 @@ if ($acesso) {
                     $tabela->set_titulo("Candidatos Aprovados");
                     $tabela->set_subtitulo($subtitulo);
                     $tabela->set_conteudo($row);
-                    $tabela->set_label(["#", "Situação", "Inscrição", "Candidato", "Nascimento", "Cota", "Nota Final", "Obs", "Editar"]);
+                    $tabela->set_label(["#", "Situação", "Inscrição", "Candidato", "Nascimento", "Classificação na Cota", "Nota Final", "Obs", "Editar"]);
                     $tabela->set_width([5, 10, 10, 30, 10, 10, 15]);
                     $tabela->set_align(["center", "center", "center", "left", "center"]);
                     $tabela->set_funcao([null, null, null, "plm", "date_to_php"]);

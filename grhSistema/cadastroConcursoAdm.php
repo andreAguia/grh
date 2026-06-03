74b873fe6ae47a3e49f7e0f82947a89a39938ba8
André Águia <alataguia@gmail.com>
2026-06-03 11:28:49 -0300

    declaração de posse

--- grhSistema/cadastroConcursoAdm.php
+++ grhSistema/cadastroConcursoAdm.php
@@ -632,7 +632,6 @@ if ($acesso) {
             if ($parametroLotacao <> "*") {
                 $lista = new ListaServidores("{$parametroLotacao} - Servidores Ativos");
                 $lista->set_lotacao($parametroLotacao);
-                $lista->set_ordenacao("4 asc");
                 $atividade = "Visualizou os servidores ativos da lotação " . $parametroLotacao . " do concurso " . $concurso->get_nomeConcurso($idConcurso);
             } else {
                 $lista = new ListaServidores("Servidores Ativos");
@@ -1200,7 +1199,7 @@ if ($acesso) {
             $lista->set_concurso($idConcurso);
             if ($parametroLotacao <> "*") {
                 $lista->set_lotacao($parametroLotacao);   
-                $lista->set_ordenacao("4 asc");
+                $lista->set_ordenacao("DIR asc,GER asc");
             }
             $lista->set_agrupamento(3);
             $lista->showRelatorio();

b1028a206ac8e5ae474e9271fc0ef71778489492
André Águia <alataguia@gmail.com>
2026-06-03 11:45:08 -0300

    declaração de posse

--- grhRelatorios/estatutarios.ativos.porLotacao.porConcurso.php
+++ grhRelatorios/estatutarios.ativos.porLotacao.porConcurso.php
@@ -52,7 +52,7 @@ if ($acesso) {
                  AND tbhistlot.data = (select max(data) from tbhistlot where tbhistlot.idServidor = tbservidor.idServidor)';
 
     # lotacao
-    if (!is_null($parametroLotacao)) {
+    if (!is_null($parametroLotacao) OR $parametroLotacao <> "*") {
         # Verifica se o que veio é numérico
         if (is_numeric($parametroLotacao)) {
             $select .= ' AND (tblotacao.idlotacao = "' . $parametroLotacao . '")';

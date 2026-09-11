8fd96b186f35d08305e8106e79be084b30d705bc
André Águia <alataguia@gmail.com>
2026-09-11 14:15:11 -0300

    melhoria na rotina de alteração de lotação

--- grhSistema/servidorLotacao.php
+++ grhSistema/servidorLotacao.php
@@ -168,7 +168,7 @@ if ($acesso) {
     $objeto->set_idUsuario($idUsuario);
     $objeto->set_idServidorPesquisado($idServidorPesquisado);
     
-    $mensagem = "Atenção!<br/> - Toda alteração de lotação de servidor deverá ser informada aos administradores do SEI na Uenf através do e-mail sei@uenf.br"
+    $mensagem = " - Toda alteração de lotação de servidor deverá ser informada aos administradores do SEI na Uenf através do e-mail sei@uenf.br"
             . "<br/> - As movimentações de lotação anteriores a 10/11/2016 não estão exatas pois são anteriores ao sistema atual e o sistema antigo não armazenava o histórico das lotações.";
 
     ################################################################
@@ -176,7 +176,7 @@ if ($acesso) {
     switch ($fase) {
         case "" :
         case "listar" :
-            $objeto->set_rotinaExtraListar("callout");
+            $objeto->set_rotinaExtraListar("calloutWarning");
             $objeto->set_rotinaExtraListarParametro($mensagem);
             $objeto->listar();
             break;
